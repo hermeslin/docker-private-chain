04428746b63e0edfe9b676807643251a354fba31
hermeslin <mercury.iwork@gmail.com>
2018-09-26 18:22:48 +0800

    fix shell script

--- bootnode/prepare/entrypoint.sh
+++ bootnode/prepare/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ ! -d /root/store/bootnode.key ]; then
-     echo "/root/store/bootnode.key not found, running 'bootnode -nodekey'..."
-     bootnode -genkey /root/store/bootnode.key
-     bootnode -nodekey /root/store/bootnode.key -writeaddress > /root/store/bootnode.address
+if [ ! -f /root/store/bootnode.key ]; then
+    echo "/root/store/bootnode.key not found, running 'bootnode -nodekey'..."
+    bootnode -genkey /root/store/bootnode.key
+    bootnode -nodekey /root/store/bootnode.key -writeaddress > /root/store/bootnode.address
     echo "...done!"
 fi
 
