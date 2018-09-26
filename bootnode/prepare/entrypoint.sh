96d16b4ff109fc3206a969c4eb11d861d5a6fa0b
hermeslin <mercury.iwork@gmail.com>
2018-09-26 17:26:03 +0800

    更換 bootnode 的 key 產生方式
    改由 bootnode -genkey /root/store/bootnode.key 產生

--- bootnode/prepare/entrypoint.sh
+++ bootnode/prepare/entrypoint.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-bootnode "$@"
+if [ ! -d /root/store/bootnode.key ]; then
+     echo "/root/store/bootnode.key not found, running 'bootnode -nodekey'..."
+     bootnode -genkey /root/store/bootnode.key
+     bootnode -nodekey /root/store/bootnode.key -writeaddress > /root/store/bootnode.address
+    echo "...done!"
+fi
+
+bootnode "$@" -nodekey /root/store/bootnode.key
