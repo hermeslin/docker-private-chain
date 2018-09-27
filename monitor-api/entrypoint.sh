31d1569edc400062044d842788a51668720d53a7
hermeslin <mercury.iwork@gmail.com>
2018-09-27 13:14:28 +0800

    add eth-netstats monitor

---
+++ monitor-api/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+NODE_ENV=production \
+RPC_HOST=$MACHINE_TYPE \
+RPC_PORT=8400 \
+WS_SERVER=http://monitor-view:3000 \
+WS_SECRET=$MONITOR_VIEW_SECRET \
+npm start --prefix /root/eth-net-intelligence-api
---
+++ monitor-view/entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+WS_SECRET=$MONITOR_VIEW_SECRET npm start --prefix /root/eth-netstats
