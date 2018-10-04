f0f0cfa8f7ac732661c21fd81c994a086ced723e
hermeslin <mercury.iwork@gmail.com>
2018-10-04 15:35:48 +0800

    feature: fix alpine version to 3.8
    alpine version is 3.8

--- geth/entrypoint.sh
+++ geth/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 DATA_DIR="/root/store/.ethereum"
+DAG_DIR="/root/store/.ethash"
+
 if [ ! -d ${DATA_DIR}/keystore ]; then
     echo "${DATA_DIR}/keystore not found, running 'geth init'..."
     geth --datadir "${DATA_DIR}" init root/genesis.json
@@ -10,4 +12,4 @@ fi
 cp /root/static-nodes.json ${DATA_DIR}
 
 ## start geth
-geth "$@" -nodekey /root/node.key --ipcpath "/root/geth.ipc" --datadir "${DATA_DIR}"
+geth "$@" -nodekey /root/node.key --ipcpath "/root/geth.ipc" --datadir "${DATA_DIR}"  --ethash.dagdir "${DAG_DIR}"
